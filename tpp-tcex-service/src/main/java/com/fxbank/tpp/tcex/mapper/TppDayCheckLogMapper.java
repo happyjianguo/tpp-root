b6ec5e00087d9d7df70c18a5155283a0d5e743d5
duzhenduo <duzhenduo@126.com>
2018-12-17 15:28:49 +0800

    添加TppDayCheckLogMapper泛型

--- tpp-tcex-service/src/main/java/com/fxbank/tpp/tcex/mapper/TppDayCheckLogMapper.java
+++ tpp-tcex-service/src/main/java/com/fxbank/tpp/tcex/mapper/TppDayCheckLogMapper.java
@@ -4,6 +4,6 @@ import com.fxbank.cip.base.common.MyMapper;
 import com.fxbank.tpp.tcex.entity.TppDayCheckLog;
 import java.util.List;
 
-public interface TppDayCheckLogMapper extends MyMapper {
+public interface TppDayCheckLogMapper extends MyMapper<TppDayCheckLog> {
     List<TppDayCheckLog> selectAll();
 }
