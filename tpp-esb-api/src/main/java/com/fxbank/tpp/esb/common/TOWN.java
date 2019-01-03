71f239a494323fa4247c1b75f2aff534a172cc56
duzhenduo <duzhenduo@126.com>
2019-01-03 09:47:42 +0800

    修改村镇mac工作密钥节点

--- tpp-esb-api/src/main/java/com/fxbank/tpp/esb/common/TOWN.java
+++ tpp-esb-api/src/main/java/com/fxbank/tpp/esb/common/TOWN.java
@@ -4,8 +4,8 @@ import com.fxbank.cip.base.constant.CIP;
 
 public class TOWN extends CIP{
 
-	public static final String macDeginId = "TASS";
-	public static final String macNodeId = "tassToesb";
+	public static final String macDeginId = "CZBK";
+	public static final String macNodeId = "czbkTotass";
 	public static final String macKeyModelId = "RZAK";
 	public static final String macPlaceHolder = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
 
