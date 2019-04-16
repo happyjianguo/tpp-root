a4019a87c4bef59869f7df86acc4b7e94d995db6
duzhenduo <duzhenduo@126.com>
2019-04-16 10:53:00 +0800

    本行柜面接口和交行接口输入输出

---
+++ tpp-bocm-api/src/main/java/com/fxbank/tpp/bocm/model/REQ_10001.java
@@ -0,0 +1,317 @@
+package com.fxbank.tpp.bocm.model;
+
+import com.fxbank.cip.base.log.MyLog;
+
+/** 
+* @ClassName: REQ_10001 
+* @Description: 磁条卡通兑 
+* @author Duzhenduo
+* @date 2019年4月16日 上午9:23:02 
+*  
+*/
+public class REQ_10001 extends REQ_BASE {
+
+	private static final long serialVersionUID = -5030423745594921356L;
+
+	private String ccyCod = "CNY";
+    
+    private String txnAmt;
+    
+    private String pin;
+
+    private String oprFlg;
+    
+    private String txnMod;
+    
+    private String payBnk;
+    
+    private String pActTp;
+    
+    private String pActNo;
+    
+    private String payNam;
+    
+    private String payAdr;
+    
+    private String recBnk;
+    
+    private String rActTp;
+    
+    private String rActNo;
+    
+    private String recNam;
+    
+    private String recAdr;
+    
+    private String cuIdTp;
+    
+    private String cuIdNo;
+    
+    private String agIdTp;
+    
+    private String agIdNo;
+    
+    private String agtNam;
+    
+    private String secMag;
+    
+    private String thdMag;
+    
+    private String remark;
+    
+    @Deprecated
+	public REQ_10001() {
+		super(null, 0, 0, 0);
+	}
+
+    public REQ_10001(MyLog mylog, Integer sysDate, Integer sysTime, Integer sysTraceno) {
+        super(mylog, sysDate, sysTime, sysTraceno);
+        super.getHeader().settTxnCd("10001");
+    }
+
+    @Override
+    public String creaFixPack() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(super.getHeader().creaFixPack());
+        sb.append(String.format("%-3s", this.ccyCod==null?"":this.ccyCod));
+        sb.append(String.format("%-15s", this.txnAmt==null?"":this.txnAmt));
+        sb.append(String.format("%-20s", this.pin==null?"":this.pin));
+        sb.append(String.format("%-1s", this.oprFlg==null?"":this.oprFlg));
+        sb.append(String.format("%-1s", this.txnMod==null?"":this.txnMod));
+        sb.append(String.format("%-12s", this.payBnk==null?"":this.payBnk));
+        sb.append(String.format("%-1s", this.pActTp==null?"":this.pActTp));
+        sb.append(String.format("%-32s", this.pActNo==null?"":this.pActNo));
+        sb.append(String.format("%-30s", this.payNam==null?"":this.payNam));
+        sb.append(String.format("%-60s", this.payAdr==null?"":this.payAdr));
+        sb.append(String.format("%-12s", this.recBnk==null?"":this.recBnk));
+        sb.append(String.format("%-1s", this.rActTp==null?"":this.rActTp));
+        sb.append(String.format("%-32s", this.rActNo==null?"":this.rActNo));
+        sb.append(String.format("%-30s", this.recNam==null?"":this.recNam));
+        sb.append(String.format("%-60s", this.recAdr==null?"":this.recAdr));
+        sb.append(String.format("%-2s", this.cuIdTp==null?"":this.cuIdTp));
+        sb.append(String.format("%-30s", this.cuIdNo==null?"":this.cuIdNo));
+        sb.append(String.format("%-2s", this.agIdTp==null?"":this.agIdTp));
+        sb.append(String.format("%-30s", this.agIdNo==null?"":this.agIdNo));
+        sb.append(String.format("%-30s", this.agtNam==null?"":this.agtNam));
+        sb.append(String.format("%-37s", this.secMag==null?"":this.secMag));
+        sb.append(String.format("%-104s", this.thdMag==null?"":this.thdMag));
+        sb.append(String.format("%-60s", this.remark==null?"":this.remark));
+        
+        return sb.toString();
+    }
+
+    @Override
+    public void chanFixPack(String pack) {
+        StringBuffer sb = new StringBuffer(pack);
+        int i = 0;
+        super.getHeader().chanFixPack(sb.substring(0, i=i+60));
+        this.ccyCod = sb.substring(i, i=i+3).trim();
+        this.txnAmt = sb.substring(i, i=i+15).trim();
+        this.pin = sb.substring(i, i=i+20).trim();
+        this.oprFlg = sb.substring(i, i=i+1).trim();
+        this.txnMod = sb.substring(i, i=i+1).trim();
+        this.payBnk = sb.substring(i, i=i+12).trim();
+        this.pActTp = sb.substring(i, i=i+1).trim();
+        this.pActNo = sb.substring(i, i=i+32).trim();
+        this.payNam = sb.substring(i, i=i+30).trim();
+        this.payAdr = sb.substring(i, i=i+60).trim();
+        this.recBnk = sb.substring(i, i=i+12).trim();
+        this.rActTp = sb.substring(i, i=i+1).trim();
+        this.rActNo = sb.substring(i, i=i+32).trim();
+        this.recNam = sb.substring(i, i=i+30).trim();
+        this.recAdr = sb.substring(i, i=i+60).trim();
+        this.cuIdTp = sb.substring(i, i=i+2).trim();
+        this.cuIdNo = sb.substring(i, i=i+30).trim();
+        this.agIdTp = sb.substring(i, i=i+2).trim();
+        this.agIdNo = sb.substring(i, i=i+30).trim();
+        this.agtNam = sb.substring(i, i=i+30).trim();
+        this.secMag = sb.substring(i, i=i+37).trim();
+        this.thdMag = sb.substring(i, i=i+104).trim();
+        this.remark = sb.substring(i, i=i+60).trim();
+    }
+
+	public String getCcyCod() {
+		return ccyCod;
+	}
+
+	public void setCcyCod(String ccyCod) {
+		this.ccyCod = ccyCod;
+	}
+
+	public String getTxnAmt() {
+		return txnAmt;
+	}
+
+	public void setTxnAmt(String txnAmt) {
+		this.txnAmt = txnAmt;
+	}
+
+	public String getOprFlg() {
+		return oprFlg;
+	}
+
+	public void setOprFlg(String oprFlg) {
+		this.oprFlg = oprFlg;
+	}
+
+	public String getTxnMod() {
+		return txnMod;
+	}
+
+	public void setTxnMod(String txnMod) {
+		this.txnMod = txnMod;
+	}
+
+	public String getPayBnk() {
+		return payBnk;
+	}
+
+	public void setPayBnk(String payBnk) {
+		this.payBnk = payBnk;
+	}
+
+	public String getpActTp() {
+		return pActTp;
+	}
+
+	public void setpActTp(String pActTp) {
+		this.pActTp = pActTp;
+	}
+
+	public String getpActNo() {
+		return pActNo;
+	}
+
+	public void setpActNo(String pActNo) {
+		this.pActNo = pActNo;
+	}
+
+	public String getPayNam() {
+		return payNam;
+	}
+
+	public void setPayNam(String payNam) {
+		this.payNam = payNam;
+	}
+
+	public String getPayAdr() {
+		return payAdr;
+	}
+
+	public void setPayAdr(String payAdr) {
+		this.payAdr = payAdr;
+	}
+
+	public String getRecBnk() {
+		return recBnk;
+	}
+
+	public void setRecBnk(String recBnk) {
+		this.recBnk = recBnk;
+	}
+
+	public String getrActTp() {
+		return rActTp;
+	}
+
+	public void setrActTp(String rActTp) {
+		this.rActTp = rActTp;
+	}
+
+	public String getrActNo() {
+		return rActNo;
+	}
+
+	public void setrActNo(String rActNo) {
+		this.rActNo = rActNo;
+	}
+
+	public String getRecNam() {
+		return recNam;
+	}
+
+	public void setRecNam(String recNam) {
+		this.recNam = recNam;
+	}
+
+	public String getRecAdr() {
+		return recAdr;
+	}
+
+	public void setRecAdr(String recAdr) {
+		this.recAdr = recAdr;
+	}
+
+	public String getCuIdTp() {
+		return cuIdTp;
+	}
+
+	public void setCuIdTp(String cuIdTp) {
+		this.cuIdTp = cuIdTp;
+	}
+
+	public String getCuIdNo() {
+		return cuIdNo;
+	}
+
+	public void setCuIdNo(String cuIdNo) {
+		this.cuIdNo = cuIdNo;
+	}
+
+	public String getAgIdTp() {
+		return agIdTp;
+	}
+
+	public void setAgIdTp(String agIdTp) {
+		this.agIdTp = agIdTp;
+	}
+
+	public String getAgIdNo() {
+		return agIdNo;
+	}
+
+	public void setAgIdNo(String agIdNo) {
+		this.agIdNo = agIdNo;
+	}
+
+	public String getAgtNam() {
+		return agtNam;
+	}
+
+	public void setAgtNam(String agtNam) {
+		this.agtNam = agtNam;
+	}
+
+	public String getSecMag() {
+		return secMag;
+	}
+
+	public void setSecMag(String secMag) {
+		this.secMag = secMag;
+	}
+
+	public String getThdMag() {
+		return thdMag;
+	}
+
+	public void setThdMag(String thdMag) {
+		this.thdMag = thdMag;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+}
---
+++ tpp-bocm-web/src/main/java/com/fxbank/tpp/bocm/dto/esb/ESB_REP_30061800201.java
@@ -0,0 +1,75 @@
+package com.fxbank.tpp.bocm.dto.esb;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fxbank.cip.base.log.MyLog;
+import com.fxbank.cip.base.model.ESB_BASE;
+import com.fxbank.cip.base.model.ESB_REP_APP_HEAD;
+import com.fxbank.cip.base.model.ESB_REP_SYS_HEAD;
+
+public class ESB_REP_30061800201 extends ESB_BASE {
+
+
+	//private static final long serialVersionUID = -8845727634828642523L;
+
+	@Deprecated
+	public ESB_REP_30061800201() {
+		super(null, 0, 0, 0);
+	}
+
+	public ESB_REP_30061800201(MyLog mylog, Integer sysDate, Integer sysTime, Integer sysTraceno) {
+		super(mylog, sysDate, sysTime, sysTraceno);
+	}
+
+	@JSONField(name = "APP_HEAD")
+	private ESB_REP_APP_HEAD repAppHead = new ESB_REP_APP_HEAD();
+
+	@JSONField(name = "SYS_HEAD")
+	private ESB_REP_SYS_HEAD repSysHead = new ESB_REP_SYS_HEAD();
+
+	@JSONField(name = "BODY")
+	private REP_BODY repBody;
+
+	public class REP_BODY implements Serializable {
+
+		//private static final long serialVersionUID = 4285318811572896212L;
+		@JSONField(name = "FEE_T3")
+		private String feeT3;//手续费
+		public String  getFeeT3(){
+			return feeT3;
+		}
+		public void setFeeT3(String feeT3){
+			this.feeT3 = feeT3;
+		}
+		
+	}
+	
+	
+
+	public REP_BODY getRepBody() {
+		return repBody;
+	}
+
+	public void setRepBody(REP_BODY repBody) {
+		this.repBody = repBody;
+	}
+
+	public ESB_REP_APP_HEAD getRepAppHead() {
+		return repAppHead;
+	}
+
+	public void setRepAppHead(ESB_REP_APP_HEAD repAppHead) {
+		this.repAppHead = repAppHead;
+	}
+
+	public ESB_REP_SYS_HEAD getRepSysHead() {
+		return repSysHead;
+	}
+
+	public void setRepSysHead(ESB_REP_SYS_HEAD repSysHead) {
+		this.repSysHead = repSysHead;
+	}
+
+}
