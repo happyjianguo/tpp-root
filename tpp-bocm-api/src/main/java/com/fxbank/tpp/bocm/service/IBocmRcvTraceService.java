bd33a873e75b21ccd2d6f59d9ba6b7d82ee4eac2
yepuliang <win915@163.com>
2019-05-22 10:41:30 +0800

    修改来往账接口赋值

--- tpp-bocm-api/src/main/java/com/fxbank/tpp/bocm/service/IBocmRcvTraceService.java
+++ tpp-bocm-api/src/main/java/com/fxbank/tpp/bocm/service/IBocmRcvTraceService.java
@@ -8,12 +8,15 @@
 */
 package com.fxbank.tpp.bocm.service;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import com.fxbank.cip.base.exception.SysTradeExecuteException;
 import com.fxbank.cip.base.log.MyLog;
 import com.fxbank.tpp.bocm.model.BocmRcvTraceInitModel;
 import com.fxbank.tpp.bocm.model.BocmRcvTraceQueryModel;
+import com.fxbank.tpp.bocm.model.BocmRcvTraceRepModel;
 import com.fxbank.tpp.bocm.model.BocmRcvTraceUpdModel;
 
 /** 
@@ -56,6 +59,142 @@ public interface IBocmRcvTraceService {
 	* @return RcvTraceQueryModel    返回类型 
 	* @throws 
 	*/
-	public BocmRcvTraceQueryModel getConfirmTrace(MyLog myLog,String townDate,String townTraceno)throws SysTradeExecuteException;
+	public BocmRcvTraceQueryModel getConfirmTrace(MyLog myLog,int townDate,String townTraceno)throws SysTradeExecuteException;
+	
+	/** 
+	* @Title: getRcvTrace 
+	* @Description: 来账查询
+	* @param @param myLog
+	* @param @param begDate
+	* @param @param endDate
+	* @param @param minAmt
+	* @param @param maxAmt
+	* @param @param brnoFlag
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return List<RcvTraceQueryModel>    返回类型 
+	* @throws 
+	*/
+	public List<BocmRcvTraceQueryModel> getRcvTrace(MyLog myLog,String begDate,String endDate,String minAmt,String maxAmt,String brnoFlag) throws SysTradeExecuteException;
+
+	/** 
+	* @Title: getCheckRcvTrace 
+	* @Description: 来账对账查询 
+	* @param @param myLog
+	* @param @param sysDate
+	* @param @param sysTime
+	* @param @param sysTraceno
+	* @param @param date
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return List<RcvTraceQueryModel>    返回类型 
+	* @throws 
+	*/
+	public List<BocmRcvTraceQueryModel> getCheckRcvTrace(MyLog myLog, Integer sysDate, Integer sysTime, Integer sysTraceno, String date)throws SysTradeExecuteException;
+
+	/** 
+	* @Title: replenishRcvTrace 
+	* @Description: 来账补账 
+	* @param @param record
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return void    返回类型 
+	* @throws 
+	*/
+	void replenishRcvTrace(BocmRcvTraceRepModel record)throws SysTradeExecuteException;
+
+	/** 
+	* @Title: getRcvTraceByKey 
+	* @Description: 通过主键查询来账 
+	* @param @param myLog
+	* @param @param sysTime
+	* @param @param sysTraceno
+	* @param @param sysDate
+	* @param @param settleDate
+	* @param @param platTrace
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return RcvTraceQueryModel    返回类型 
+	* @throws 
+	*/
+	BocmRcvTraceQueryModel getRcvTraceByKey(MyLog myLog, Integer sysTime, Integer sysTraceno, Integer sysDate,
+			Integer settleDate, Integer platTrace)throws SysTradeExecuteException;
+	
+	/** 
+	* @Title: getBocmRcvTraceByKey 
+	* @Description: 通过交行日期流水查询来账 
+	* @param @param myLog
+	* @param @param sysTime
+	* @param @param sysTraceno
+	* @param @param sysDate
+	* @param @param settleDate
+	* @param @param platTrace
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return RcvTraceQueryModel    返回类型 
+	* @throws 
+	*/
+	BocmRcvTraceQueryModel getBocmRcvTraceByKey(MyLog myLog, Integer sysTime, Integer sysTraceno, Integer sysDate,
+			Integer bocmDate, String bocmTrace)throws SysTradeExecuteException;
+
+	/** 
+	* @Title: getUploadCheckRcvTrace 
+	* @Description: 查询下载的来账对账文件
+	* @param @param myLog
+	* @param @param sysDate
+	* @param @param sysTime
+	* @param @param sysTraceno
+	* @param @param date
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return List<RcvTraceQueryModel>    返回类型 
+	* @throws 
+	*/
+	List<BocmRcvTraceQueryModel> getUploadCheckRcvTrace(MyLog myLog, Integer sysDate, Integer sysTime, Integer sysTraceno,
+			String date)throws SysTradeExecuteException;
+	/** 
+	* @Title: getRcvTotalNum 
+	* @Description: 查询来账对账总数 
+	* @param @param myLog
+	* @param @param date
+	* @param @param dcFlag
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return String    返回类型 
+	* @throws 
+	*/
+	String getRcvTotalNum(MyLog myLog, String date,String dcFlag) throws SysTradeExecuteException;
+	/** 
+	* @Title: getRcvTotalSum 
+	* @Description: 查询来账对账总金额 
+	* @param @param myLog
+	* @param @param date
+	* @param @param dcFlag
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return String    返回类型 
+	* @throws 
+	*/
+	String getRcvTotalSum(MyLog myLog, String date,String dcFlag) throws SysTradeExecuteException;
+	/**
+	 * 获取来账统计信息
+	 * @param date
+	 * @param checkFlag
+	 * @return
+	 * @throws SysTradeExecuteException
+	 */
+	String getTraceNum(String date,String checkFlag)throws SysTradeExecuteException;
+	/** 
+	* @Title: getRcvTotalSum 
+	* @Description: 查询来账对账总金额 
+	* @param @param myLog
+	* @param @param date
+	* @param @param dcFlag
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return String    返回类型 
+	* @throws 
+	*/
+	String getRcvTotalChkSum(MyLog myLog, String date) throws SysTradeExecuteException;
+	
 
 }
--- tpp-bocm-api/src/main/java/com/fxbank/tpp/bocm/service/IBocmSndTraceService.java
+++ tpp-bocm-api/src/main/java/com/fxbank/tpp/bocm/service/IBocmSndTraceService.java
@@ -1,11 +1,14 @@
 package com.fxbank.tpp.bocm.service;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import com.fxbank.cip.base.exception.SysTradeExecuteException;
 import com.fxbank.cip.base.log.MyLog;
 import com.fxbank.tpp.bocm.model.BocmSndTraceInitModel;
 import com.fxbank.tpp.bocm.model.BocmSndTraceQueryModel;
+import com.fxbank.tpp.bocm.model.BocmSndTraceRepModel;
 import com.fxbank.tpp.bocm.model.BocmSndTraceUpdModel;
 
 /** 
@@ -51,5 +54,109 @@ public interface IBocmSndTraceService {
 	*/
 	BocmSndTraceQueryModel getSndTraceByKey(MyLog myLog, Integer sysTime, Integer sysTraceno, Integer sysDate,
 			Integer settleDate, Integer platTrace) throws SysTradeExecuteException;
+	/** 
+	* @Title: getSndTraceByKey 
+	* @Description: 通过主键查询往账流水
+	* @param @param myLog
+	* @param @param sysTime
+	* @param @param sysTraceno
+	* @param @param sysDate
+	* @param @param settleDate
+	* @param @param platTrace
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return SndTraceQueryModel    返回类型 
+	* @throws 
+	*/
+	BocmSndTraceQueryModel getBocmSndTraceByKey(MyLog myLog, Integer sysTime, Integer sysTraceno, Integer sysDate,
+			Integer bocmDate, String bocmTraceno) throws SysTradeExecuteException;
+	/** 
+	* @Title: getSndTrace 
+	* @Description: 查询往账流水 
+	* @param @param myLog
+	* @param @param begDate
+	* @param @param endDate
+	* @param @param minAmt
+	* @param @param maxAmt
+	* @param @param brnoFlag
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return List<SndTraceQueryModel>    返回类型 
+	* @throws 
+	*/
+	public List<BocmSndTraceQueryModel> getSndTrace(MyLog myLog,String begDate,String endDate,String minAmt,String maxAmt,String brnoFlag) throws SysTradeExecuteException;
 
+	/** 
+	* @Title: replenishSndTrace 
+	* @Description: 往账补账 
+	* @param @param record
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return void    返回类型 
+	* @throws 
+	*/
+	void replenishSndTrace(BocmSndTraceRepModel record) throws SysTradeExecuteException;
+
+	/** 
+	* @Title: getCheckSndTrace 
+	* @Description: 查询往账对账
+	* @param @param myLog
+	* @param @param sysDate
+	* @param @param sysTime
+	* @param @param sysTraceno
+	* @param @param date
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return List<SndTraceQueryModel>    返回类型 
+	* @throws 
+	*/
+	List<BocmSndTraceQueryModel> getCheckSndTrace(MyLog myLog, Integer sysDate, Integer sysTime, Integer sysTraceno,
+			String date) throws SysTradeExecuteException;
+
+	/** 
+	* @Title: getUploadCheckSndTrace 
+	* @Description: 查询下载的往账对账文件
+	* @param @param myLog
+	* @param @param sysDate
+	* @param @param sysTime
+	* @param @param sysTraceno
+	* @param @param date
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return List<SndTraceQueryModel>    返回类型 
+	* @throws 
+	*/
+	List<BocmSndTraceQueryModel> getUploadCheckSndTrace(MyLog myLog, Integer sysDate, Integer sysTime, Integer sysTraceno,
+			String date) throws SysTradeExecuteException;
+	/** 
+	* @Title: getSndTotalNum 
+	* @Description: 查询往账对账总数
+	* @param @param myLog
+	* @param @param date
+	* @param @param dcFlag
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return String    返回类型 
+	* @throws 
+	*/
+	String getSndTotalNum(MyLog myLog, String date,String dcFlag) throws SysTradeExecuteException;
+	/** 
+	* @Title: getSndTotalSum 
+	* @Description: 查询往账对账总金额
+	* @param @param myLog
+	* @param @param date
+	* @param @param dcFlag
+	* @param @return
+	* @param @throws SysTradeExecuteException    设定文件 
+	* @return String    返回类型 
+	* @throws 
+	*/
+	String getSndTotalSum(MyLog myLog, String date,String dcFlag) throws SysTradeExecuteException;
+	/**
+	 * 统计往帐条数
+	 * @param date
+	 * @param checkFlag
+	 * @return
+	 * @throws SysTradeExecuteException
+	 */
+	String getTraceNum(String date, String checkFlag) throws SysTradeExecuteException;
 }
