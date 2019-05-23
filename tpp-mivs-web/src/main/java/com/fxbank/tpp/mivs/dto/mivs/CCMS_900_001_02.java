b80f233ecf8e8cf8c93ce7e028b357468cb2ef5f
wangpeng <zhouyongwei@126.com>
2019-05-23 10:21:22 +0800

    tpp-mivs-web/dto/mivs/CCMS_900_001_02

--- tpp-mivs-web/src/main/java/com/fxbank/tpp/mivs/dto/mivs/CCMS_900_001_02.java
+++ tpp-mivs-web/src/main/java/com/fxbank/tpp/mivs/dto/mivs/CCMS_900_001_02.java
@@ -1,6 +1,7 @@
 package com.fxbank.tpp.mivs.dto.mivs;
 
-import com.fxbank.tpp.mivs.model.response.CCMS_900_001_02_CmonConf;
+import com.fxbank.tpp.mivs.model.CCMS_900_001_02_CmonConf;
+import com.fxbank.tpp.mivs.model.CCMS_990_001_02_ComConf;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -14,24 +15,41 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "Document")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CCMS_900_001_02 extends DTO_BASE {
-    private static final long serialVersionUID = -4125716324225975090L;
-    private CCMS_900_001_02_CmonConf CmonConf = new CCMS_900_001_02_CmonConf();
+    private static final long serialVersionUID = 3833828532687872260L;
+    private CCMS_990_001_02_ComConf comConf = new CCMS_990_001_02_ComConf();
 
     public CCMS_900_001_02() {
-        super.txDesc = "通用处理确认报文900";
+        super.txDesc = "通用处理确认报文CCMS.900.001.02";
+    }
+
+    private CCMS_900_001_02_CmonConf CmonConf = new CCMS_900_001_02_CmonConf();
+
+    /**
+     * @return the comConf
+     */
+    public CCMS_990_001_02_ComConf getComConf() {
+        return this.comConf;
+    }
+
+    /**
+     * @param comConf the comConf to set
+     */
+    public void setComConf(CCMS_990_001_02_ComConf comConf) {
+        this.comConf = comConf;
+    }
+
+    @Override
+    public String signData() {
+        return this.comConf.signData();
     }
 
     public CCMS_900_001_02_CmonConf getCmonConf() {
         return CmonConf;
     }
 
-    public void setCmonConf(CCMS_900_001_02_CmonConf cmonConf) {
-        CmonConf = cmonConf;
+    public void setCmonConf(CCMS_900_001_02_CmonConf comConf) {
+        CmonConf = comConf;
     }
 
-    @Override
-    public String signData() {
-        return this.CmonConf.signData();
-    }
 
 }
