287d3f6eda4ed5461f2771d32f7284700bf3a9a1
yepuliang <win915@163.com>
2019-06-04 09:03:06 +0800

    添加加密引用jar包

---
+++ tpp-bocm-api/src/main/java/com/fxbank/tpp/bocm/common/ESB.java
@@ -0,0 +1,12 @@
+package com.fxbank.tpp.bocm.common;
+
+import com.fxbank.cip.base.constant.CIP;
+
+public class ESB extends CIP{
+
+	public static final String macDeginId = "CIP";
+	public static final String macNodeId = "cipToesb";
+	public static final String macKeyModelId = "RZAK";
+	public static final String macPlaceHolder = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+
+}
