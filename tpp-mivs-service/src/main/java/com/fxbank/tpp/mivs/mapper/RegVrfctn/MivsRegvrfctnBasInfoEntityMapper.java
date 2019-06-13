246c142219b7a79c164b21e2a90648b166de4f49
wangpeng <zhouyongwei@126.com>
2019-06-13 11:54:47 +0800

    Mapper

--- tpp-mivs-service/src/main/java/com/fxbank/tpp/mivs/mapper/RegVrfctn/MivsRegvrfctnBasInfoEntityMapper.java
+++ tpp-mivs-service/src/main/java/com/fxbank/tpp/mivs/mapper/RegVrfctn/MivsRegvrfctnBasInfoEntityMapper.java
@@ -2,7 +2,6 @@ package com.fxbank.tpp.mivs.mapper.RegVrfctn;
 
 import com.fxbank.cip.base.common.MyMapper;
 import com.fxbank.tpp.mivs.entity.RegVrfctn.MivsRegvrfctnBasInfoEntity;
-
 import java.util.List;
 
 public interface MivsRegvrfctnBasInfoEntityMapper extends MyMapper<MivsRegvrfctnBasInfoEntity> {
