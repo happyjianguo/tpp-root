ad9f276f9198b8f2a24cf4efaa51688dc3244ac3
yepuliang <win915@163.com>
2019-06-25 08:38:06 +0800

    修改获取交行 密钥定时

--- tpp-manager/src/main/java/com/fxbank/tpp/manager/quartz/ApplyWorkKeyTask.java
+++ tpp-manager/src/main/java/com/fxbank/tpp/manager/quartz/ApplyWorkKeyTask.java
@@ -32,6 +32,7 @@ import com.fxbank.cip.base.exception.SysTradeExecuteException;
 import com.fxbank.cip.base.log.MyLog;
 import com.fxbank.cip.pub.service.IPublicService;
 import com.fxbank.tpp.bocm.model.REP_10104_MAC;
+import com.fxbank.tpp.bocm.model.REP_10104_PIN;
 import com.fxbank.tpp.bocm.model.REQ_10103;
 import com.fxbank.tpp.bocm.model.REQ_10104;
 import com.fxbank.tpp.bocm.service.IBocmSafeService;
@@ -85,22 +86,21 @@ public class ApplyWorkKeyTask {
 		Date d = df.parse(sysDate.toString());     
 		Calendar cal=Calendar.getInstance();
 		cal.setTime(d);
-		//TODO  调试期间用当天后期修改
-//		cal.add(Calendar.DATE, -1);  //减1天
 		
 		Integer date = Integer.parseInt(df.format(cal.getTime()));
 		Integer sysTime = publicService.getSysTime();
 		Integer sysTraceno = publicService.getSysTraceno();
-		
-		
+				
 		//交行总行行号
 		String JHNO = "";
 		//阜新银行总行行号
 		String FXNO = "";
+		String KeyId = "";
 		try(Jedis jedis = myJedis.connect()){
 			//从redis中获取交行总行行号
 			JHNO = jedis.get(COMMON_PREFIX+"JHNO");
 			FXNO = jedis.get(COMMON_PREFIX+"FXNO");
+			KeyId = jedis.get(COMMON_PREFIX+"KeyId");
         }
 		
 		//1.申请MAC key
@@ -108,48 +108,41 @@ public class ApplyWorkKeyTask {
 		reqMac10104.setSbnkNo(FXNO);
 		reqMac10104.setRbnkNo(FXNO);
 		//密钥ID
-		reqMac10104.setKeyId("RZAK");
+		reqMac10104.setKeyId(KeyId);
 		//密钥类型
 		reqMac10104.setKeyTyp(1);
 		//密钥长度
 		reqMac10104.setKeyLen(16);
 		myLog.info(logger, "请求Mac密钥");
-		com.fxbank.tpp.bocm.model.REP_10104_MAC repMac10104 = forwardToBocmService.sendToBocm(reqMac10104, com.fxbank.tpp.bocm.model.REP_10104_MAC.class);
+		REP_10104_MAC repMac10104 = forwardToBocmService.sendToBocm(reqMac10104, REP_10104_MAC.class);
 		
 		String macKeyValue = repMac10104.getBlkVal();
 		String macCheckValue = repMac10104.getChkVal();
-		myLog.info(logger, "Mac密钥密文值【"+macCheckValue+"】密钥校验值【"+macCheckValue+"】");
+		
+		myLog.info(logger, "Mac密钥密文值【"+macKeyValue+"】密钥校验值【"+macCheckValue+"】");
 		//Mac密钥 更新
-		//safeService.updateMacKey(myLog, macKeyValue, macCheckValue);
+		safeService.updateMacKey(myLog, macKeyValue, macCheckValue);
 		
 		
 		//1.申请Pin key
 		REQ_10104 reqPin10104 = new REQ_10104(myLog, date, sysTime, sysTraceno);
+		reqPin10104.setSbnkNo(FXNO);
+		reqPin10104.setRbnkNo(FXNO);
 		//密钥ID
-		reqPin10104.setKeyId("RZPK");
+		reqPin10104.setKeyId(KeyId);
 		//密钥类型
 		reqPin10104.setKeyTyp(0);
 		//密钥长度
 		reqPin10104.setKeyLen(32);
 		myLog.info(logger, "请求Pin密钥");
-		com.fxbank.tpp.bocm.model.REP_10104_MAC repPin10104 = forwardToBocmService.sendToBocm(reqPin10104, com.fxbank.tpp.bocm.model.REP_10104_MAC.class);
+		REP_10104_PIN repPin10104 = forwardToBocmService.sendToBocm(reqPin10104, REP_10104_PIN.class);
 		
 		String pinKeyValue = repPin10104.getBlkVal();
-		String pinCheckValue = repMac10104.getChkVal();
-		myLog.info(logger, "Pin密钥密文值【"+macCheckValue+"】密钥校验值【"+macCheckValue+"】");
-		//Pin密钥 更新
-		//safeService.updateMacKey(myLog, pinKeyValue, pinCheckValue);
-		
-		
-//		try(Jedis jedis = myJedis.connect()){
-//			//把从交行请求的key存储在redis中
-//			jedis.set(COMMON_PREFIX+"BLKVALUE", keyValue);
-//			//把从交行请求的校验码存储在redis中
-//			jedis.set(COMMON_PREFIX+"CHKVALUE", checkValue);
-//        }
+		String pinCheckValue = repPin10104.getChkVal();
+		myLog.info(logger, "Pin密钥密文值【"+pinKeyValue+"】密钥校验值【"+pinCheckValue+"】");
+		safeService.updatePinKey(myLog, pinKeyValue, pinCheckValue);
 		myLog.info(logger, "密钥定时更新完毕");
-//		myLog.info(logger, "新申请的加密密钥："+keyValue);
-//		myLog.info(logger, "新申请的校验码："+checkValue);
+
 	}
 	
 	
