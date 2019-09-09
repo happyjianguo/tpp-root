4d16329c7d10d7aec88a31cdb24d2e977f0e1d31
wangpeng <zhouyongwei@126.com>
2019-09-09 20:49:54 +0800

    20190909 修改应答911 redis订阅通道

--- tpp-mivs-web/src/main/java/com/fxbank/tpp/mivs/trade/mivs/DscrdMsgNtfctn.java
+++ tpp-mivs-web/src/main/java/com/fxbank/tpp/mivs/trade/mivs/DscrdMsgNtfctn.java
@@ -38,6 +38,21 @@ public class DscrdMsgNtfctn extends TradeBase implements TradeExecutionStrategy 
         myLog.info(logger, "收到人行报文丢弃通知报文,进行同步处理");
         byte[] b911 = SerializeUtil.serialize(ccms911);
         String msgType = ccms911.getDscrdMsgNtfctn().getDscrdInf().getMT().substring(5, 8);
+        if(msgType.equals("320")){
+            msgType = "321";
+        }else if(msgType.equals("322")){
+            msgType = "323";
+        }else if(msgType.equals("324")){
+            msgType = "325";
+        }else if(msgType.equals("326")){
+            msgType = "900";
+        }else if(msgType.equals("333")){
+            msgType = "900";
+        }else if(msgType.equals("345")){
+            msgType = "346";
+        }else if(msgType.equals("347") || msgType.equals("348") || msgType.equals("349")){
+            msgType = "900";
+        }
         String channel = msgType + "_" + ccms911.getDscrdMsgNtfctn().getDscrdInf().getMsgId(); // TODO 拼接原报文三要素
         myLog.info(logger, "911报文同步通道编号=[" + channel + "]");
         super.jedisPublish(myLog,channel.getBytes(), b911);
