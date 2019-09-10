ecf354e4017e21368f27e8a9f7fe576d226c783f
yepuliang <win915@163.com>
2019-09-10 09:18:36 +0800

    有风险控制版本

--- tpp-bocm-web/src/main/java/com/fxbank/tpp/bocm/trade/esb/TradeBase.java
+++ tpp-bocm-web/src/main/java/com/fxbank/tpp/bocm/trade/esb/TradeBase.java
@@ -106,7 +106,6 @@ public class TradeBase {
 	//风险检查
 	public void riskCheck(MyLog myLog,DataTransObject dto,String payerAcno,String payeeAcno,Long amt,
 			String bizChnl,String bizCode) throws SysTradeExecuteException{
-		/**
 		// 增加风险监控 检查20190809 begin
 		REQ_FRMS frmsModel = new REQ_FRMS(myLog, dto.getSysDate(), dto.getSysTime(), dto.getSysTraceno());
 		//业务渠道    String  O-柜面通
@@ -128,12 +127,10 @@ public class TradeBase {
 			throw new BocmTradeExecuteException(BocmTradeExecuteException.BOCM_E_10019);
 		}
 		// 增加风险监控 检查20190809 end	
-		 */
 	}
 	//状态通知
 	public void statusNotify(MyLog myLog,DataTransObject dto,String payerAcno,String payeeAcno,Long amt
 			,String bizChnl,String bizCode,String operStatus,String respCode) throws SysTradeExecuteException{
-		/**
 		// 增加风险监控 检查20190809 begin
 		REQ_FRMS frmsModel = new REQ_FRMS(myLog, dto.getSysDate(), dto.getSysTime(), dto.getSysTraceno());
 		frmsModel.setSerialId(dto.getSysDate()+String.valueOf(dto.getSysTraceno()));
@@ -160,6 +157,5 @@ public class TradeBase {
 		if(frmsRep.getVerifyPolicy()!=null){
 			myLog.info(logger, "风险监控应答  Code："+frmsRep.getVerifyPolicy().getCode()+"  Name:"+frmsRep.getVerifyPolicy().getName());
 		}
-		*/
 	}
 }
