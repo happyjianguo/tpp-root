ab06baa0c4b83f0f3ac30b8435aafd8ae9c8a202
yepuliang <win915@163.com>
2019-09-24 15:52:25 +0800

    风险控制屏蔽

--- tpp-bocm-web/src/main/java/com/fxbank/tpp/bocm/trade/esb/TradeBase.java
+++ tpp-bocm-web/src/main/java/com/fxbank/tpp/bocm/trade/esb/TradeBase.java
@@ -106,6 +106,7 @@ public class TradeBase {
 	//风险检查
 	public void riskCheck(MyLog myLog,DataTransObject dto,String payerAcno,String payeeAcno,Long amt,
 			String bizChnl,String bizCode) throws SysTradeExecuteException{
+		/*
 		// 增加风险监控 检查20190809 begin
 		REQ_FRMS frmsModel = new REQ_FRMS(myLog, dto.getSysDate(), dto.getSysTime(), dto.getSysTraceno());
 		frmsModel.setSerialId(dto.getSysDate()+String.valueOf(dto.getSysTraceno()));
@@ -141,6 +142,7 @@ public class TradeBase {
 			throw new BocmTradeExecuteException(BocmTradeExecuteException.BOCM_E_10019);
 		}
 		// 增加风险监控 检查20190809 end	
+		 */
 	}
 	//状态通知
 	public void statusNotify(MyLog myLog,DataTransObject dto,String payerAcno,String payeeAcno,Long amt
